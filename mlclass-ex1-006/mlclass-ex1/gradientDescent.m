964b0cc251efea95f7b0936fd6cd266c8e1483c7
young-geng <young.gengxy@gmail.com>
2014-06-25 00:31:09 -0700

    ex1

--- mlclass-ex1-006/mlclass-ex1/gradientDescent.m
+++ mlclass-ex1-006/mlclass-ex1/gradientDescent.m
@@ -16,8 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    
-
+    theta = theta - sum(transpose(X) * (X * theta - y)) / m * alpha;
 
 
 
